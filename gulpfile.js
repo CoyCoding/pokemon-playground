6571b93542037853ead7929aff16d4687bfb9495
Matt Coy <fluffytoycoy@gmail.com>
2019-05-24 02:45:17 -0500

    gulp piping of img js and scss test and working

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,22 @@ const babel = require('gulp-babel');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 
-gulp.task('default', series(styles, scripts, sync, (done) => {
+gulp.task('default', series(styles, scripts, sync, img, (done) => {
   gulp.watch('./assets/sass/**/*', series(styles));
   gulp.watch('./assets/js/**/*', series(scripts));
   gulp.watch(['./public/**/*', './public/*', '!public/js/**/.#*js', '!public/css/**/.#*css']).on('change', reload)
+  gulp.watch('./assets/img/**/*', series(img));
   done();
 }))
 
+function img(){
+  return gulp.src('./assets/img/**/*')
+  .pipe(gulp.dest('./public/img/'))
+   .pipe(browserSync.stream());
+};
 
+gulp.task(img);
+gulp.task(scripts)
 function scripts() {
  return gulp.src('./assets/js/**/*.js')
      .pipe(
@@ -38,7 +46,13 @@ function styles() {
    .pipe(browserSync.stream())
 };
 
-
+function sync(){
+  return  browserSync.init({
+        server: './public',
+        notify: false,
+        open: false //change this to true if you want the broser to open automatically
+    });
+};
 
 //
 // gulp.task('browser-sync', ['styles'], function () {
@@ -51,13 +65,7 @@ function styles() {
 //   //   serveStatic: ['.', './public']
 //   // })
 //
-function sync(){
-  return  browserSync.init({
-        server: './public',
-        notify: false,
-        open: false //change this to true if you want the broser to open automatically
-    });
-};
+
 
 
 

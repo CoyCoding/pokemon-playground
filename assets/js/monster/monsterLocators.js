25c104b383822a926a73d003a912f20cd5b00596
Matt Coy <fluffytoycoy@gmail.com>
2019-05-25 19:53:43 -0500

    added new monster builder class for better structure.

--- assets/js/monster/monsterLocators.js
+++ assets/js/monster/monsterLocators.js
@@ -1,24 +1,49 @@
 import { monsterList } from './data/monsterList';
 import { Monster } from './modles/monster';
+import { MonsterBuilder } from './MonsterBuilder';
 
-export function findMonsterById(id) {
-	const foundMonster = monsterList.find(monster => {
-		return id === monster.id;
-	});
-	if (foundMonster) {
-		return new Monster(
-			foundMonster.id,
-			foundMonster.name,
-			foundMonster.types,
-			foundMonster.img,
-			foundMonster.learnableMoves,
-			foundMonster.movePool
-		);
-	} else {
-		throw new Error('monster not found');
+export const MonsterLocator = {
+	// buildMonster: function(object) {
+	// 	try {
+	// 		return new Monster(
+	// 			object.id,
+	// 			object.name,
+	// 			object.types,
+	// 			object.img,
+	// 			object.learnableMoves,
+	// 			object.movePool
+	// 		);
+	// 	} catch (e) {
+	// 		console.log('bad build');
+	// 	}
+	// },
+
+	findMonsterById: function(id) {
+		const foundMonster = monsterList.find(monster => {
+			return id === monster.id;
+		});
+		if (foundMonster) {
+			return MonsterBuilder.buildMonster(foundMonster);
+		} else {
+			throw new Error('monster not found');
+		}
+	},
+
+	monsterListLength: function() {
+		return monsterList.length;
+	},
+
+	findRandomMonster: function() {
+		let index = Math.floor(Math.random() * this.monsterListLength());
+		return MonsterBuilder.buildMonster(this.findMonsterByIndex(index));
+	},
+	findMonsterByIndex: function(index) {
+		if (index < this.monsterListLength() && index >= 0) {
+			return monsterList[index];
+		} else {
+			throw new Error('bad Index');
+		}
 	}
-}
 
-export function monsterListLength() {
-	return monsterList.length;
-}
+	//// TODO: Make MonsterBuilder class
+};

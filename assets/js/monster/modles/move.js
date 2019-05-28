f686cdad963c5afbf396069117dd121dde8350b5
Matt Coy <fluffytoycoy@gmail.com>
2019-05-28 03:42:04 -0500

    .gitignore is now working

--- assets/js/monster/modles/move.js
+++
@@ -1,7 +0,0 @@
-export class Move {
-	constructor(id, name, type) {
-		this.id = id;
-		this.name = name;
-		this.type = type;
-	}
-}
--- assets/js/monster/typeLocators.js
+++
@@ -1,8 +0,0 @@
-import {typeList} from './data/typeList'
-import { Type } from './modles/type'
-
-// export function findTypeName(name){
-//   const foundType = typeList.find(type=>{
-//     return type[name];
-//   })
-// }

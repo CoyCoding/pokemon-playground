81bdcbb999f943cde5ab14596aca29b268873fab
Matt Coy <fluffytoycoy@gmail.com>
2019-05-31 12:43:00 -0500

    added draggable monster cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,108 +2,77 @@ import { MonsterDom } from './monster/monsterDom';
 import { MonsterLocator } from './monster/monsterLocators';
 import { MoveLocator } from './monster/moveLocator';
 const PokemonApi = require('pokeapi-js-wrapper');
-import Draggabilly from 'draggabilly';
-const $ = require('jquery');
+// import Draggabilly from 'draggabilly';
+// const jquery = require('jquery');
+// window.$ = window.jQuery = jquery;
+const shapeshift = require('./shifter');
 
-var draggableElems = document.querySelectorAll('.draggable');
-var draggies = [];
+$('.gridly').shapeshift({
+	align: 'center',
+	gutterX: 0,
+	gutterY: 0,
+	paddingX: 0,
+	handle: false
+});
 
-for (var i = 0, len = draggableElems.length; i < len; i++) {
-	var draggieElem = draggableElems[i];
-	var draggie = new Draggabilly(draggieElem, {
-		containment: '.grid'
-	});
-	var grid = [482.27, 502];
-	console.log(draggie);
-	draggie.on('dragEnd', function(event) {
-		console.log(event);
-		console.log(this.position);
-		this.position.x = applyGrid(this.position.x, grid[0]);
+const options = {
+	protocol: 'https',
+	versionPath: '/api/v2/',
+	cache: true,
+	timeout: 5 * 1000 // 5s
+};
 
-		this.position.y = applyGrid(this.position.y, grid[1]);
-		this.setLeftTop();
-	});
+var monsterLocator = new MonsterLocator(new PokemonApi.Pokedex(options));
+var moveLocator = new MoveLocator(monsterLocator.api());
+const pokeballBtn = document.getElementsByClassName('ball-btn');
 
-	function applyGrid(value, grid) {
-		return Math.round(value / grid) * grid;
+initPokeballs();
+
+// console.log(monsterLocator.locateRandomPokemon());
+
+function initPokeballs() {
+	for (let i = 0; i < pokeballBtn.length; i++) {
+		pokeballBtn[i].addEventListener('click', placePokemonInBall);
 	}
-	draggies.push(draggie);
+
+	function placePokemonInBall() {
+		this.removeEventListener('click', placePokemonInBall);
+		let currentImgNode = getClosest(this, '.poke-img');
+		let currentNameNode = getClosest(this, '.name-wrapper');
+		let currentTypeNode = getClosest(this, '.poke-types');
+		let currentMoveNode = getClosest(this, '.moves');
+		monsterLocator.locateRandomPokemon().then(monster => {
+			moveLocator.locateRandomMoveSet(4, monster.learnableMoves).then(res => {
+				Promise.all(res)
+					.then(res => {
+						monster.setMovePool(res);
+					})
+					.then(() => {
+						MonsterDom.setNameNode(monster, currentNameNode);
+						MonsterDom.setTypeNodes(monster, currentTypeNode);
+						MonsterDom.setMoveNodes(monster.movePool, currentMoveNode);
+						MonsterDom.setImageNode(monster.img, currentImgNode);
+						initNodes([currentNameNode, currentTypeNode, currentMoveNode]);
+						getCurrentPokeBall(this).classList.add('opened');
+						currentImgNode.classList.add('opened');
+					});
+			});
+		});
+	}
+
+	function getClosest(node, className) {
+		return node.closest('.monster-info').querySelector(className);
+	}
+
+	function getCurrentPokeBall(node) {
+		return node.closest('.ball');
+	}
+
+	function initNodes(nodeArr) {
+		nodeArr.forEach(node => {
+			node.classList.add('loaded');
+		});
+	}
 }
-//
-// const options = {
-// 	protocol: 'https',
-// 	versionPath: '/api/v2/',
-// 	cache: true,
-// 	timeout: 5 * 1000 // 5s
-// };
-//
-// var monsterLocator = new MonsterLocator(new PokemonApi.Pokedex(options));
-// var moveLocator = new MoveLocator(monsterLocator.api());
-// const pokeballBtn = document.getElementsByClassName('ball-btn');
-//
-// initPokeballs();
-//
-// // console.log(monsterLocator.locateRandomPokemon());
-//
-// function initPokeballs() {
-// 	for (let i = 0; i < pokeballBtn.length; i++) {
-// 		pokeballBtn[i].addEventListener('click', placePokemonInBall);
-// 	}
-//
-// 	function placePokemonInBall() {
-// 		this.removeEventListener('click', placePokemonInBall);
-// 		let currentImgNode = getClosest(this, '.poke-img');
-// 		let currentNameNode = getClosest(this, '.name-wrapper');
-// 		let currentTypeNode = getClosest(this, '.poke-types');
-// 		let currentMoveNode = getClosest(this, '.moves');
-// 		monsterLocator.locateRandomPokemon().then(monster => {
-// 			moveLocator.locateRandomMoveSet(4, monster.learnableMoves).then(res => {
-// 				Promise.all(res)
-// 					.then(res => {
-// 						monster.setMovePool(res);
-// 					})
-// 					.then(() => {
-// 						MonsterDom.setNameNode(monster, currentNameNode);
-// 						MonsterDom.setTypeNodes(monster, currentTypeNode);
-// 						MonsterDom.setMoveNodes(monster.movePool, currentMoveNode);
-// 						MonsterDom.setImageNode(monster.img, currentImgNode);
-// 						initNodes([currentNameNode, currentTypeNode, currentMoveNode]);
-// 						getCurrentPokeBall(this).classList.add('opened');
-// 						currentImgNode.classList.add('opened');
-// 					});
-// 			});
-// 		});
-// 	}
-//
-// 	function getClosest(node, className) {
-// 		return node.closest('.monster-info').querySelector(className);
-// 	}
-//
-// 	function getCurrentPokeBall(node) {
-// 		return node.closest('.ball');
-// 	}
-//
-// 	function initNodes(nodeArr) {
-// 		nodeArr.forEach(node => {
-// 			node.classList.add('loaded');
-// 		});
-// 	}
-// }
-//
-// //Action/Event based functions;
-// function createPokemonTeam() {
-// 	//creates a 6 monster team with no moves.
-// 	// return MonsterGenerator.generateMonsters(6);
-// }
-//
-// function createMovePoolFor(monsterArr) {
-// 	//Gives monster or array of monsters a move P
-// 	//MonsterModifier.generateRandomMoves(monsterArr, 4);
-// }
-//
-// function createMonsterWithMoves() {
-// 	// var monster = MonsterGenerator.generateMonsters(1);
-// 	// createMovePoolFor(monster);
-// 	// return monster;
-// }
-// //HTML based functions
+
+//HTML based functions
